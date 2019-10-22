9a515a3c06b54baf7da7d63a0d46a5b5f563d9ce
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-10-22 16:28:44 +0200

    feat: Add middleware

---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+const logger = store => next => action => {
+  console.group(action.type);
+  console.log("Action: ", action);
+  const result = next(action);
+  console.log("Result: ", store.getState());
+  console.groupEnd();
+};
+
+export default logger;
