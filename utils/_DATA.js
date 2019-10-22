b78a7911fd7722f2efdfecd36d96240bbbd8a7cf
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-10-22 10:18:05 +0200

    feat: Add api

--- utils/_DATA.js
+++ utils/_DATA.js
@@ -23,3 +23,73 @@ let decks = {
     ]
   }
 };
+
+export function _getDecks() {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({ ...decks }), 1000);
+  });
+}
+
+export function _getDeckById(id) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(decks[id]);
+    }, 1000);
+  });
+}
+
+export function _saveDeck(title) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      decks = {
+        ...decks,
+        [title]: {
+          title: title,
+          questions: []
+        }
+      };
+
+      res(decks[title]);
+    }, 1000);
+  });
+}
+
+export function _deleteDeck(title) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      decks[title] = {};
+      delete decks[title];
+      res(decks);
+    }, 1000);
+  });
+}
+
+export function _addCard(question, deck) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      decks = {
+        ...decks,
+        [deck]: {
+          ...decks[deck],
+          questions: decks[deck].questions.concat([question])
+        }
+      };
+      res(decks[deck]);
+    }, 1000);
+  });
+}
+
+export function _deleteCard(question, deck) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      decks = {
+        ...decks,
+        [deck]: {
+          ...decks[deck],
+          questions: decks[deck].questions.filter(q => question !== q.question)
+        }
+      };
+      res(decks[deck]);
+    }, 1000);
+  });
+}
